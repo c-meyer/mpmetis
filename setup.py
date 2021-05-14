11dcc57412fd7fd7afb8e6729f72f3636a7283ad
Christian Meyer <christian.meyer@tum.de>
2021-05-14 19:45:51 +0200

    Added setup.cfg and setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+macros = ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")
+
+extensions = [
+    Extension("ammetis.mod_part_mesh_nodal",
+              ["src/ammetis/mod_part_mesh_nodal.pyx"],
+              include_dirs=["metis-5.1.0/include"],
+              libraries=["metis"],
+              library_dirs=["lib/lib"],
+              define_macros=[macros]),
+]
+setup(
+    ext_modules=cythonize(extensions),
+)
