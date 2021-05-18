b15f349a1f5c8a74635b73d0ad1bc0326778abe2
Christian Meyer <christian.meyer@tum.de>
2021-05-18 11:40:57 +0200

    Added install command in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import sys
+from os.path import join
+import fileinput
 import subprocess
 from setuptools import Extension, setup
 
@@ -8,13 +10,19 @@ BUILD_METIS = False
 args = sys.argv[1:]
 build_commands = ('develop', 'build', 'build_ext', 'build_py',
                   'build_clib', 'build_scripts', 'bdist_wheel', 'bdist_rpm',
-                  'bdist_wininst', 'bdist_msi', 'bdist_mpkg')
+                  'bdist_wininst', 'bdist_msi', 'bdist_mpkg', 'install')
 
 for command in build_commands:
     if command in args:
         BUILD_METIS = True
 
 if BUILD_METIS:
+    # Replace real data type to 64 bit (double)
+    metis_h_fname = join('metis-5.1.0', 'include', 'metis.h')
+    for line in fileinput.input(metis_h_fname, inplace=True):
+        if line.startswith('#define REALTYPEWIDTH'):
+            line = line.replace('32', '64')
+        print('{}'.format(line), end='')
     if sys.platform.startswith('linux'):
         subprocess.run(['sh', 'build.sh'], check=True)
     elif sys.platform.startswith('darwin'):
