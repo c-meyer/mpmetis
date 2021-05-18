7a98531f13f6080ae87ba3b809ba7d5aa7122ca5
Christian Meyer <christian.meyer@tum.de>
2021-05-18 10:45:16 +0200

    Try to fix build process

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 import sys
+import subprocess
 from setuptools import Extension, setup
 
+BUILD_METIS = False
 
-macros = ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")
 
 args = sys.argv[1:]
+build_commands = ('develop', 'build', 'build_ext', 'build_py',
+                  'build_clib', 'build_scripts', 'bdist_wheel', 'bdist_rpm',
+                  'bdist_wininst', 'bdist_msi', 'bdist_mpkg')
+
+for command in build_commands:
+    if command in args:
+        BUILD_METIS = True
+
+if BUILD_METIS:
+    if sys.platform.startswith('linux'):
+        subprocess.run('build.sh')
+    elif sys.platform.startswith('darwin'):
+        subprocess.run('build.sh')
+    else:
+        raise NotImplementedError('Your operating system is not supported'
+                                  'by setup.py. Try to build the package'
+                                  'by yourself.')
+
 if 'sdist' in args:
     IS_SDIST = True
     from Cython.Build import cythonize
@@ -13,6 +32,7 @@ else:
     IS_SDIST = False
     srcfile = "src/mpmetis/mod_part_mesh_nodal.c"
 
+macros = ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")
 extensions = [Extension("mpmetis.mod_part_mesh_nodal",
                         [srcfile],
                         include_dirs=["metis-5.1.0/include"],
